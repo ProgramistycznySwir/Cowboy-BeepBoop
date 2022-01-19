eeb9d1099724e0bc4b7531576cdba280885009e1
DwieStronyMocy <igomidormid@gmail.com>
2022-01-19 18:27:42 +0100

    Working start game button

---
+++ Cowbot Beep-Boop/Assets/GameSceneManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    
+}
