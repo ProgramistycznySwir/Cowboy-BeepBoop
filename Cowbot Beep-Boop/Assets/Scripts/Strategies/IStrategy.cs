2e01f56044868feb4d7e657f9555bece3e8233bc
Daniel Pietrzeniuk <ProgramistycznySwir@gmail.com>
2022-01-25 00:49:55 +0100

    and

--- Cowbot Beep-Boop/Assets/Scripts/Strategies/IStrategy.cs
+++ Cowbot Beep-Boop/Assets/Scripts/Strategies/IStrategy.cs
@@ -9,5 +9,15 @@ namespace Cowbot_Beep_Boop.Strategies
         Color GetColor();
         Vector2 Fire(Vector2 position, Vector2 playerPosition);
         (float throttle, float steering) Move(Vector2 position, Vector2 forward, Vector2 playerPosition);
+
+        public IStrategy GetStrategy(StrategyType type)
+            => type switch {
+                StrategyType.Default => new DefaultStrategy(),
+                StrategyType.ChargeAtEnemy => new ChargeAtEnemyStrategy(),
+                StrategyType.KeepDistance => new KeepDistanceStrategy(),
+                _ => new DefaultStrategy()
+            };
     }
+
+    public enum StrategyType { Default, ChargeAtEnemy, KeepDistance }
 }
